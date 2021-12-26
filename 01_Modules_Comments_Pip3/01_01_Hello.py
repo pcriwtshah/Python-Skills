034de064d7f2b744978d03494b08a14788f41517
Pashupati Shah <pashupatishah@yahoo.com>
2021-12-26 17:13:58 -0500

    updated the program

--- 01_Modules_Comments_Pip3/01_01_Hello.py
+++ 01_Modules_Comments_Pip3/01_01_Hello.py
@@ -2,6 +2,11 @@
 Author: Pashupati Shah
 Licensed to : 
 '''
+#To write a comment:
+#use pound symbol in front of the line OR use '''   ''' like below
+'''
+All the text inside ''' ''' is comment
+'''
 
 #Here we will use the print() and input() function
 print("Hello World")
