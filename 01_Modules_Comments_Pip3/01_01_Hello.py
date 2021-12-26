b1a7fe8738f12aa4222e934332243caafacc6637
Pashupati Shah <pashupatishah@yahoo.com>
2021-12-26 17:08:52 -0500

    First Program

---
+++ 01_Modules_Comments_Pip3/01_01_Hello.py
@@ -0,0 +1,17 @@
+'''
+Author: Pashupati Shah
+Licensed to : 
+'''
+
+#Here we will use the print() and input() function
+print("Hello World")
+
+#Welcome people
+name = input("Enter your name: ")
+print("Welcome to learn Python,", name)
+
+
+
+#This is not needed but to show how to import modules
+import os  #importing os module
+
