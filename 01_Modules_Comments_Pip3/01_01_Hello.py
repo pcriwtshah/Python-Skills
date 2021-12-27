130fe562fa2c86db502fe282587d7ad5e6d23dbd
Pashupati Shah <pashupatishah@yahoo.com>
2021-12-27 13:41:07 -0500

    updated

--- 01_Modules_Comments_Pip3/01_01_Hello.py
+++ 01_Modules_Comments_Pip3/01_01_Hello.py
@@ -5,7 +5,10 @@ Licensed to :
 #To write a comment:
 #use pound symbol in front of the line OR use '''   ''' like below
 '''
-All the text inside ''' ''' is comment
+All the text inside ''' ''' is comment.
+
+Comments are bits of text in the program that the computer ignores. 
+`They are there solely for the benefit of any human readers.
 '''
 
 #Here we will use the print() and input() function
